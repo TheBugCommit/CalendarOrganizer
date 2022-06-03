d937ddd44c476720e49af048561893b4ac6e2244
Gerard Casas Serarols <gerard@databis.net>
2022-06-03 15:35:53 +0200

    cahnges

--- app/Http/Controllers/JasperReportController.php
+++ app/Http/Controllers/JasperReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -17,17 +18,20 @@ class JasperReportController extends Controller
 
     public function getReport(Request $request)
     {
-        $url = 'http://daw2-gcasas:2737C@51.68.224.27:8080/jasperserver/rest_v2/reports/daw2-gcasas/sintesis.pdf?email=' . Auth::user()->email ;
+        $url = 'http://daw2-gcasas:2737C@51.68.224.27:8080/jasperserver/rest_v2/reports/daw2-gcasas/sintesis.pdf?email=' . Auth::user()->email;
 
-        if(isset($request->categories) && $request->categories != null){
-            foreach($request->categories as $category_name){
+        if (isset($request->categories) && $request->categories != null) {
+            foreach ($request->categories as $category_name) {
                 $url .= "&category_names=$category_name";
             }
         }
 
-
-        $tempfile = file_get_contents($url);
-        Storage::put("report.pdf", $tempfile);
-        return response()->file(storage_path() . "/app/report.pdf");
+        try {
+            $tempfile = file_get_contents($url);
+            Storage::put("report.pdf", $tempfile);
+            return response()->file(storage_path() . "/app/report.pdf");
+        } catch (Exception $ex) {
+            abort(404);
+        }
     }
 }
